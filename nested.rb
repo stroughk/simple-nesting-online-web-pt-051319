55852ea88b69aca5ab3530d9819a71dbe884bb0d
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-26 19:49:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,9 +105,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for].replace"GUI"   #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+        #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay]
      
 end
 
